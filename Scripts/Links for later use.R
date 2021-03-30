c012f2505676abf2e2847aa35e55366a72025352
Tim Stephensgit config -- global user.email tim.stephens@mail.utoronto.ca <tim.stephens@mail.utoronto.ca>
2021-03-29 19:45:29 -0700

    Tim Commit March 29 745PM

---
+++ Scripts/Links for later use.R
@@ -0,0 +1,26 @@
+# # LINKS THAT I HAVE USED
+# 
+# 
+# Gasoline Report 
+# https://open.canada.ca/data/en/dataset/db90f229-be24-468a-bbce-46f710e7af76
+# 
+# 
+# New Motor Vehicle Sales
+# https://open.canada.ca/data/en/dataset/28caed1c-12cf-48ea-80b3-2a518cd58be8
+# 
+# 
+# US Gas prices
+# https://www.energy.gov/eere/vehicles/downloads/fact-915-march-7-2016-average-historical-annual-gasoline-pump-price-1929 
+# 
+# 
+# Historic OPEC prices 
+# https://www.mwv.de/statistiken/rohoelpreise/
+#   
+#   
+# Statsita OPEC 
+# https://www-statista-com.myaccess.library.utoronto.ca/statistics/262858/change-in-opec-crude-oil-prices-since-1960/ 
+#   
+
+
+# More world oil prices
+# https://ourworldindata.org/grapher/crude-oil-prices
