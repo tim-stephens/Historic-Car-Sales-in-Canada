2c81f5ff2966a420d6dcf7ba46763654953bb27a
Tim Stephensgit config -- global user.email tim.stephens@mail.utoronto.ca <tim.stephens@mail.utoronto.ca>
2021-04-22 23:29:21 -0700

    1130 PST TRS APR22

---
+++ Scripts/Extra thoughts.R
@@ -0,0 +1,15 @@
+#possible reason why oil prices are correlated (+) with auto sales: 
+
+#A possible explanation for this correlation is as follows: as more Canadians 
+#buy cars, cities and towns are developed around the assumption that one will 
+#own a vehicle. Towns become more spread out, cities are built around roads, 
+#and soon, it is essential for one to own an automobile. Since a car is now a
+#necessity, and a car runs on gas, oil producers can raise the price of oil, 
+#knowing that Canadians will have to fork out the extra cash to keep their car
+#running. As cities continue develop, more Canadians buy cars, and the cycle
+#continues. Looking at Figures 3 and 4, it is clear that both car sales and oil
+#prices increase over time. 
+
+# This correlation makes sense on the larger scale, but not in the time frame the 
+# study was inspecting. Perhaps long term, oil prices and car sales are positively 
+# correlated, and the early 1980s was an anomaly. 
